a920bd763c46d0c64e0ca5891024e22d87dd779d
Rahul Agarwal <rahulaga@amazon.com>
2020-04-10 13:38:58 -0700

    Dark Mode

--- src/App.js
+++ src/App.js
@@ -2,39 +2,57 @@ import React from 'react';
 import './App.css';
 import Link from './components/Link';
 import Rotator from './components/Rotator';
+import Toggle from './components/Toggle';
 import Resume from './documents/resume.pdf';
 
-function App() {
-  return (
-    <div className="App">
-      <header>
-        <section className="left">
-          <div id="my-dash"></div>
-        </section>
-        <section className="right">
-          <Link href={Resume}>Resume</Link>
-        </section>
-      </header>
-      <section className="primary">
-          <p id="my-name">Rahul Agarwal</p>
-          <Rotator elements={["Developer", "Designer", "Learner"]}></Rotator>
-      </section>
-      <footer>
-        <section className="left">
-          <p id="working">Currently working for Amazon <br /> in Seattle.</p>
-        </section>
-        <section className="right">
-          <section className="account-links">
-            <Link href="https://github.com/eccentricdz">Github</Link>
-            <Link href="https://www.behance.net/eccentricdz">Behance</Link>
-            <Link href="https://dribbble.com/eccentricdz">Dribbble</Link>
-            <Link href="https://www.instagram.com/simplyrahul93/">Instagram</Link>
-            <Link href="https://www.linkedin.com/in/simplyrahul93/">Linkedin</Link>
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      mode: "light"
+    }
+
+    this.toggleMode = this.toggleMode.bind(this)
+  }
+
+  toggleMode() {
+    this.setState((state, props) => {
+      const mode = state.mode === "light" ? "dark" : "light"
+      return { mode }
+    })
+  }
+
+  render() {
+    return (
+      <div className={`${this.state.mode} App`}>
+        <header>
+          <section className="left">
+            <Toggle handleToggle={this.toggleMode}></Toggle>
           </section>
+          <section className="right">
+            <Link href={Resume}>Resume</Link>
+          </section>
+        </header>
+        <section className="primary">
+            <p id="my-name">Rahul Agarwal</p>
+            <Rotator elements={["Developer", "Designer", "Learner"]}></Rotator>
         </section>
-      </footer>
-    </div>
-  );
+        <footer>
+          <section className="left">
+            <p id="working">Currently working for Amazon <br /> in Seattle.</p>
+          </section>
+          <section className="right">
+            <section className="account-links">
+              <Link href="https://github.com/eccentricdz">Github</Link>
+              <Link href="https://www.behance.net/eccentricdz">Behance</Link>
+              <Link href="https://dribbble.com/eccentricdz">Dribbble</Link>
+              <Link href="https://www.instagram.com/simplyrahul93/">Instagram</Link>
+              <Link href="https://www.linkedin.com/in/simplyrahul93/">Linkedin</Link>
+            </section>
+          </section>
+        </footer>
+      </div>
+    );
+  }
 }
 
-export default App;
---
+++ src/components/Toggle.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Toggle.css';
+
+export default class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      switchPosition: "left"
+    }
+
+    this.toggle = this.toggle.bind(this)
+  }
+
+  toggle() {
+    this.setState((state, props) => {
+      const switchPosition = state.switchPosition === "left" ? "right" : "left"
+      return { switchPosition }
+    })
+
+    this.props.handleToggle();
+  }
+
+  render() {
+    return (
+      <div className={`${this.state.switchPosition} toggle-container`} onClick={this.toggle}>
+        <div className="toggle-switch"></div>
+      </div>
+    )
+  }
+}
