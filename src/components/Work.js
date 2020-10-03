b3646dc4be058a059c8e32149281a908bf549420
Rahul Agarwal <rahulaga@amazon.com>
2020-10-03 17:39:42 +0530

    Adding experimental useTransition and Suspense APIs from React to enable lazy laoding of images

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,39 +1,65 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useEffect, useReducer } from 'react';
 import './Work.scss'
-import Project, { ProjectSkeleton } from './Project';
+import Project from './Project';
 import Curtain from './Curtain';
-import projects from '../projects.json';
+import SkeletonLoaders from './SkeletonLoaders';
+import projectsArray from '../projects.json';
 
-export default class Work extends React.Component {
-    render() {
-        return (
-            <section className="work">
-                <Curtain></Curtain>
-                <Suspense fallback={
-                    <div className="skeleton-container">
-                        <ProjectSkeleton></ProjectSkeleton>
-                        <ProjectSkeleton></ProjectSkeleton>
-                        <ProjectSkeleton></ProjectSkeleton>
-                        <ProjectSkeleton></ProjectSkeleton>
-                    </div>
-                }>
-                { projects.map((project, index) => {
-                    const { id, description, title, url, tags, extension } = project;
-                    return (
-                        <Project
-                        key={index}
-                        id={id}
-                        title={title}
-                        description={description}
-                        tags={tags}
-                        url={url}
-                        imageExtension={ extension === undefined ? "jpg" : extension }
-                        clickHandler={(id) => this.props.clickHandler(id)}
-                        ></Project>
-                    )
-                }) }
-                </Suspense>
-            </section>
-        )
+const Work = () => {
+    const pageSize = 4;
+
+    const projectReducer = (state, action) => {
+        switch (action.type) {
+            case "INCREMENT_PAGE": {
+                return { ...state, projectCount: projectsArray.size, projects: projectsArray }
+            }
+            default: return state;
+        }
     }
+
+    const [projectInfo, projectDispatch] = useReducer(projectReducer, { projectCount: pageSize, projects: projectsArray.slice(0, pageSize) })
+    const [startTransition, isPending] = React.unstable_useTransition({ timeoutMs: 10000 });
+
+    useEffect(() => {
+        const scrollListener = () => {
+            startTransition(() => {
+                projectDispatch({ type: "INCREMENT_PAGE" });
+            })
+            document.removeEventListener("scroll", scrollListener);
+        }
+    
+        document.addEventListener("scroll", scrollListener)
+    }, [projectDispatch, startTransition])
+
+    return (
+        <section className="work">
+            <Curtain></Curtain>
+            <Suspense fallback={
+                <SkeletonLoaders count={4}></SkeletonLoaders>
+            }>
+                {
+                    projectInfo.projects.map((project, index) => {
+                        const { id, description, title, url, tags, extension } = project;
+                        return (
+                            <Project
+                                key={index}
+                                id={id}
+                                title={title}
+                                description={description}
+                                tags={tags}
+                                url={url}
+                                imageExtension={extension === undefined ? "jpg" : extension}
+                                clickHandler={(id) => this.props.clickHandler(id)}
+                            ></Project>
+                        )
+                    })
+                }
+                {
+                    isPending ? <SkeletonLoaders count={4}></SkeletonLoaders> : null
+                }
+            </Suspense>
+        </section>
+    )
 }
+
+export default Work;
--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ReactDOM.unstable_createRoot(
   document.getElementById('root')
-);
+).render(<React.StrictMode><App /></React.StrictMode>);
