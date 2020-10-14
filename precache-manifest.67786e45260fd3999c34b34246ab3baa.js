d4a02cb5ff6969019e8bddf47b98af8583f54939
Rahul Agarwal <rahulaga@amazon.com>
2020-10-14 20:15:02 +0530

    Updates

---
+++ precache-manifest.67786e45260fd3999c34b34246ab3baa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db91e6e93074ace1abdb0d071c860ce8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "faba4577b447c06c3c36",
+    "url": "/static/css/main.49e05d47.chunk.css"
+  },
+  {
+    "revision": "05250deabca7e9483345",
+    "url": "/static/js/2.4e750a59.chunk.js"
+  },
+  {
+    "revision": "a168c24010f99fb13adeefd582258e3b",
+    "url": "/static/js/2.4e750a59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faba4577b447c06c3c36",
+    "url": "/static/js/main.7a83f594.chunk.js"
+  },
+  {
+    "revision": "eb2da8a6912a8c64fb47",
+    "url": "/static/js/runtime-main.555d898a.js"
+  },
+  {
+    "revision": "aa5953ef2422ecbef777d33ddab15003",
+    "url": "/static/media/resume-design.aa5953ef.pdf"
+  }
+]);
