e4d0968895a57520d1f36e97bb25996d26280bde
Rahul Agarwal <rahul.agarwal@cloudkitchens.com>
2024-05-23 10:05:11 -0700

    Updates

---
+++ precache-manifest.f062d307c95d2b509fcd8b0143398a12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f4c4118b48042ae1a4f9dc234ca1d16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b820bf4ceaff352959f",
+    "url": "/static/css/main.dfc05b3a.chunk.css"
+  },
+  {
+    "revision": "5144426794146bc6e666",
+    "url": "/static/js/2.e336c720.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.e336c720.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b820bf4ceaff352959f",
+    "url": "/static/js/main.473f9722.chunk.js"
+  },
+  {
+    "revision": "efe25b4b8d1f7af9da4b",
+    "url": "/static/js/runtime-main.6b3488d1.js"
+  }
+]);
