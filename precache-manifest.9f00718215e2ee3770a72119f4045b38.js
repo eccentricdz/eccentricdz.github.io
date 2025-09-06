bd761fab300624497850dd8bfa46b5e51734bc16
Rahul Agarwal <simplyrahul93@gmail.com>
2025-09-05 17:02:40 -0700

    Updates

---
+++ precache-manifest.9f00718215e2ee3770a72119f4045b38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31f157429d57aed27be1dce076a9c85f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "858d47167d10c730b2e3",
+    "url": "/static/css/main.dfc05b3a.chunk.css"
+  },
+  {
+    "revision": "81398726e61673416699",
+    "url": "/static/js/2.4aae6f75.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/static/js/2.4aae6f75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "858d47167d10c730b2e3",
+    "url": "/static/js/main.006ed5fa.chunk.js"
+  },
+  {
+    "revision": "7c5e4ace7e9c5a53bd8e",
+    "url": "/static/js/runtime-main.06e93911.js"
+  }
+]);
